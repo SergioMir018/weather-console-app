90b6fdbe2fda80bd76aaa6b81fff1705de186b85
Sergio Mir <voidtracer6@gmail.com>
2023-05-22 17:40:43 -0400

    modules/search.js: added

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const {inquirerMenu, pause} = require('./helpers/inquirer');
+const Search = require('./models/search');
 
 const main = async() => {
     let opt = '';
+    const search = new Search();
 
     do {
         opt = await inquirerMenu();
+
+        switch (opt) {
+            case 1:
+                break;
+            case 2:
+                break;
+        }
 
         if (opt !== '0') await pause();
     } while (opt !== '0');
---
+++ models/search.js
@@ -0,0 +1,15 @@
+class Search {
+    constructor (history) {
+
+    }
+
+    async findCity (city) {
+
+
+
+        return [];
+    }
+
+}
+
+module.exports = Search;
